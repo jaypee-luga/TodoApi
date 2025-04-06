906a533714827c17f97c35eed8f427b22da40927
Jaypee Luga <code.euler@gmail.com>
2025-04-07 00:58:19 +0800

    add endpoints to project

--- Program.cs
+++ Program.cs
@@ -13,7 +13,19 @@ app.MapGet("/todoitems", async (TodoDb db) =>
 app.MapGet("/todoitems/complete", async (TodoDb db) =>
  await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
+app.MapGet("todoitems/{id}", async (int id, TodoDb db) =>
+ await db.Todos.FindAsync(id)
+  is Todo todo
+  ? Results.Ok(todo)
+  : Results.NotFound());
 
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
 app.MapGet("/", () => "Hello World!");
 
