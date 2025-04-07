9eff61831e957c15391d55e36cb763349298a171
Jaypee Luga <code.euler@gmail.com>
2025-04-07 17:01:43 +0800

    add new endpoints

--- Program.cs
+++ Program.cs
@@ -40,6 +40,17 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    return Results.NotFound();
+});
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
